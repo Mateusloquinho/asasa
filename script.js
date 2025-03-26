1f427aa019925a465368098e3b897b4da3e00107
Mateusloquinho <143225546+Mateusloquinho@users.noreply.github.com>
2025-03-26 11:59:12 -0300

    as

--- script.js
+++
@@ -1,8 +0,0 @@
-let materias = ["Matemática", "Português", "História", "Geografia"];
-let indiceMateria = 0;
-
-function alternarMateria() {
-    indiceMateria = (indiceMateria + 1) % materias.length;
-    const materiaTexto = document.getElementById('materia-texto');
-    materiaTexto.textContent = `Matéria atual: ${materias[indiceMateria]}`;
-}
